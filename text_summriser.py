eb6bd2254a7c35bcfff8177a0dec7cf5a5ae9f9f
ayan7344 <khana78673@gmail.com>
2025-12-04 12:43:30 +0530

    Delete text_summriser.py

--- text_summriser.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-summarizer = pipeline("summarization")
-
-text = input("Enter the text to be summarized:\n\n")
-
-summary = summarizer(text, max_length=30, min_length=30, do_sample=False)
-
-print("\nSummary:\n", summary[0]['summary_text'])
